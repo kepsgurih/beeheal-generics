f4ec99e3a7f5ff3f9c1ff151a9883b0ad171d58d
kepsgurih <kepsgurih@pm.me>
2024-11-15 20:17:06 +0700

    :wrench: test

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,7 +1,7 @@
 declare global {
     namespace Express {
       interface Request {
-        user?: any; // Anda bisa menyesuaikan tipe data user sesuai dengan struktur data yang Anda miliki
+        user?: any;
       }
     }
   }
