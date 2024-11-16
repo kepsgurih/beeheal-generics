208077e90e887bce52b643566e83996519c356b1
kepsgurih <kepsgurih@pm.me>
2024-11-16 10:46:37 +0700

    :wrench: CI/CD Test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Beeheal';
+    return 'Beeheal' + process.env.MONGODB_URI;
   }
 }
